2fb52df72c03fe90912b7ff1dc79e492278d4752
Santiago Trejos <Trejitos07@hotmail.com>
2022-05-19 18:48:26 -0500

    added current price

--- binance_spot_history_bot.py
+++ binance_spot_history_bot.py
@@ -10,7 +10,7 @@ import framework.utils.excel_utils as excel_utils
 
 
 file_name = 'spot-trade-history.xlsx'
-pairs = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'ADAUSDT']
+pairs = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'ADAUSDT', 'DOTUSDT', 'FTMUSDT']
 
 def main():
     api_key = os.getenv('BNCAK')
@@ -22,8 +22,9 @@ def main():
     init_formats(workbook)
 
     for pair in pairs:
+        current_price = round(float(client.get_avg_price(symbol='BTCUSDT')['price']), 2)
         trades = get_trade_data(client, pair)
-        create_pair_sheet(workbook, pair, trades)
+        create_pair_sheet(workbook, pair, trades, current_price)
 
     workbook.close()
     os.startfile(file_name)
@@ -40,18 +41,24 @@ def parse_trade_data(trade_data):
     quantity = float(trade_data['qty'])
     return Trade(trade_data["symbol"], side, price, quantity, date_time)
 
-def create_pair_sheet(workbook, pair, trades = None):
+def create_pair_sheet(workbook, pair, trades = None, current_price = None):
     sheet = workbook.add_worksheet(pair)
-    excel_utils.set_table_column(sheet, (0, 0), 'Date', 16)
-    excel_utils.set_table_column(sheet, (0, 1), 'Side', 10)
-    excel_utils.set_table_column(sheet, (0, 2), 'Price', 14)
-    excel_utils.set_table_column(sheet, (0, 3), 'Quantity', 12)
-    excel_utils.set_table_column(sheet, (0, 4), 'Cost', 10)
-    excel_utils.set_table_column(sheet, (0, 5), 'Total Quantity', 18)
-    excel_utils.set_table_column(sheet, (0, 6), 'Total Cost', 14)
-    excel_utils.set_table_column(sheet, (0, 7), 'Profit', 10)
-    excel_utils.set_table_column(sheet, (0, 8), 'Profit %', 10)
 
+    sheet.merge_range(0, 0, 0, 8, pair, excel_utils.book_formats['title'])
+    sheet.merge_range(1, 0, 1, 8, current_price, excel_utils.book_formats['currency-2'])
+    sheet.merge_range(2, 0, 2, 8, 'Trades', excel_utils.book_formats['title'])
+
+    excel_utils.set_table_column(sheet, (3, 0), 'Date', 16)
+    excel_utils.set_table_column(sheet, (3, 1), 'Side', 10)
+    excel_utils.set_table_column(sheet, (3, 2), 'Price', 14)
+    excel_utils.set_table_column(sheet, (3, 3), 'Quantity', 12)
+    excel_utils.set_table_column(sheet, (3, 4), 'Cost', 10)
+    excel_utils.set_table_column(sheet, (3, 5), 'Total Quantity', 18)
+    excel_utils.set_table_column(sheet, (3, 6), 'Total Cost', 14)
+    excel_utils.set_table_column(sheet, (3, 7), 'Profit', 10)
+    excel_utils.set_table_column(sheet, (3, 8), 'Profit %', 10)
+
+    table_start_row = 4
     total_quantity = 0
     total_cost = 0
 
@@ -60,28 +67,29 @@ def create_pair_sheet(workbook, pair, trades = None):
         total_quantity += trade.quantity
         total_cost += trade.cost
 
-        sheet.write(i+1, 0, trade.time.strftime("%d/%m/%Y"), excel_utils.book_formats["default"])
-        sheet.write(i+1, 1, trade.side, excel_utils.book_formats["default"])
-        sheet.write(i+1, 2, trade.price, excel_utils.book_formats["currency"])	
-        sheet.write(i+1, 3, trade.quantity, excel_utils.book_formats["default"])
-        sheet.write(i+1, 4, trade.cost, excel_utils.book_formats["currency"])
-        sheet.write(i+1, 5, total_quantity, excel_utils.book_formats["default"])
-        sheet.write(i+1, 6, total_cost, excel_utils.book_formats["currency"])
-        sheet.write(i+1, 7, "-", excel_utils.book_formats["default"])
-        sheet.write(i+1, 8, "-", excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 0, trade.time.strftime("%d/%m/%Y"), excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 1, trade.side, excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 2, trade.price, excel_utils.book_formats["currency"])	
+        sheet.write(i + table_start_row, 3, trade.quantity, excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 4, trade.cost, excel_utils.book_formats["currency"])
+        sheet.write(i + table_start_row, 5, total_quantity, excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 6, total_cost, excel_utils.book_formats["currency"])
+        sheet.write(i + table_start_row, 7, "-", excel_utils.book_formats["default"])
+        sheet.write(i + table_start_row, 8, "-", excel_utils.book_formats["default"])
 
     count = len(trades)
+    table_end_row = table_start_row + count - 1
 
-    price_range = Rect(1, 2, count - 1, 0)
-    quantity_range = Rect(1, 3, count - 1, 0)
-    cost_range = Rect(1, 4, count - 1, 0)
+    price_range = Rect(table_start_row, 2, count - 1, 0)
+    quantity_range = Rect(table_start_row, 3, count - 1, 0)
+    cost_range = Rect(table_start_row, 4, count - 1, 0)
 
-    sheet.write(count + 1, 2, excel_utils.get_avg_weighted_formula(price_range, quantity_range), excel_utils.book_formats["currency"])
-    sheet.write(count + 1, 3, excel_utils.get_sum_range_formula(quantity_range), excel_utils.book_formats["default"])
-    sheet.write(count + 1, 4, excel_utils.get_avg_weighted_formula(cost_range, quantity_range), excel_utils.book_formats["currency"])
+    sheet.write(table_end_row + 1, 2, excel_utils.get_avg_weighted_formula(price_range, quantity_range), excel_utils.book_formats["currency"])
+    sheet.write(table_end_row + 1, 3, excel_utils.get_sum_range_formula(quantity_range), excel_utils.book_formats["default"])
+    sheet.write(table_end_row + 1, 4, excel_utils.get_avg_weighted_formula(cost_range, quantity_range), excel_utils.book_formats["currency"])
 
-    color_range = excel_utils.get_range_notation(Rect(1, 0, count - 1, 8))
-    side_column_start = excel_utils.get_cell_notation(1, 1)
+    color_range = excel_utils.get_range_notation(Rect(table_start_row, 0, count - 1, 8))
+    side_column_start = excel_utils.get_cell_notation(table_start_row, 1)
 
     sheet.conditional_format(color_range,  {"type": "formula", "criteria": f'=${side_column_start}="BUY"', "format": excel_utils.book_formats["green"]})
     sheet.conditional_format(color_range,  {"type": "formula", "criteria": f'=${side_column_start}="SELL"', "format": excel_utils.book_formats["red"]})
@@ -92,8 +100,10 @@ def create_pair_sheet(workbook, pair, trades = None):
 def init_formats(workbook):
     excel_utils.init_format(workbook, 'default', {})
     excel_utils.init_format(workbook, 'title', {'bold': True, 'font_size': 14, 'bg_color': '#555555', 'font_color': '#ffffff'})
+    excel_utils.init_format(workbook, 'subtitle', {'bold': True, 'font_size': 14,})
     excel_utils.init_format(workbook, 'date', {'num_format': 'dd/mm/yyyy'})
     excel_utils.init_format(workbook, 'currency', {'num_format': '$#,##0.00'})
+    excel_utils.init_format(workbook, 'currency-2', {'num_format': '$#,##0.00', 'bold': True, 'font_size': 14})
     excel_utils.init_format(workbook, 'green', {'font_color': '#006100', 'bg_color': '#C6EFCE'})
     excel_utils.init_format(workbook, 'red', {'font_color': '#9C0006', 'bg_color': '#FFC7CE'})
 
